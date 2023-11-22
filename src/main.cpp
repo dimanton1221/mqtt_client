25a555f4aa12c40835d86d688aa5aaa7d6306b28
izzam <izzam@izzam.me>
2023-11-22 22:33:17 +0700

    bisa parkir dan mengirimkan mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,109 @@
-#include <ESP8266WiFi.h>
+#include <Arduino.h>
 #include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include <Servo.h>
 
-// Inisialisasi koneksi WiFi
+Servo Servoku;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 const char *ssid = "Barulah Kamar";
 const char *password = "gantipassword";
+const char *mqttServer = "192.168.2.100";
 
-const int ledPin = LED_BUILTIN;
+// KETERANGAN PIN
+const int Gerbang = D5;
+const int IR_Gerbang = D1;
+const int IR_Gerbang2 = D0;
+const int Parkir_1 = D3;
+const int Parkir_2 = D2;
 
-// Inisialisasi klien WiFi
-WiFiClient wifiClient;
+void setup_wifi()
+{
+  // delay(10);
+  // Serial.println();
+  // Serial.print("Mencoba terhubung ke: ");
+  // Serial.println(ssid);
+  WiFi.setHostname("Parkir");
+  WiFi.begin(ssid, password);
 
-// Inisialisasi klien MQTT
-PubSubClient mqttClient(wifiClient);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  // Serial.println("");
+  // Serial.println("WiFi connected");
+  // Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
-
-void callback(char* topic, byte* payload, unsigned int length) {
-    Serial.printf("Message arrived [%s] %.*s\n", topic, length, payload);
-
-    if (strncmp((char*)payload, "ON", length) == 0) {
-        digitalWrite(ledPin, LOW);
+void reconnect()
+{
+  while (!client.connected())
+  {
+    // Serial.print("");
+    if (client.connect("ParkiRIOT"))
+    {
+      // Serial.println("connected");
     }
-    else if (strncmp((char*)payload, "OFF", length) == 0) {
-        digitalWrite(ledPin, HIGH);
+    else
+    {
+      delay(5000);
     }
+  }
 }
 
 void setup()
 {
-    // Inisialisasi pin LED
-    pinMode(ledPin, OUTPUT);
-    // Mulai serial
-    Serial.begin(9600);
-
-    // Hubungkan ke WiFi
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-
-    // Tunggu sampai terhubung ke WiFi
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.print(".");
-    }
-
-    // Tampilkan informasi WiFi
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-    Serial.print("Gateway: ");
-    Serial.println(WiFi.gatewayIP());
-    const char *mqttServer = "192.168.2.100";
-
-    // Hubungkan ke MQTT broker
-    mqttClient.setServer(mqttServer, 1883);
-    mqttClient.setCallback(callback);
-    while (!mqttClient.connected())
-    {
-        Serial.println("Connecting to MQTT...");
-        if (mqttClient.connect("ESP8266Client"))
-        {
-            Serial.println("connected");
-            mqttClient.subscribe("lampu");
-        }
-        else
-        {
-            Serial.print("failed with state ");
-            Serial.print(mqttClient.state());
-            delay(2000);
-        }
-    }
+  const int pos = 180; // variable to store the servo position
+  client.setServer(mqttServer, 1883);
+  Servoku.attach(Gerbang);
+  Servoku.write(pos);
+  Serial.begin(9600);
+  setup_wifi();
+  reconnect();
 }
 
 void loop()
 {
-    if (!mqttClient.connected()) {
-        setup();
-    }
-    mqttClient.loop();
+
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  if (digitalRead(IR_Gerbang) == LOW || digitalRead(IR_Gerbang2) == LOW)
+  {
+    Servoku.write(0);
+    delay(1000);
+  }
+  else
+  {
+    Servoku.write(180);
+  }
+
+  if (digitalRead(Parkir_1) == LOW)
+  {
+    client.publish("Parkir/1", "1");
+    // Serial.println("Parkir 1 Terisi");
+  }
+  else
+  {
+    client.publish("Parkir/1", "0");
+    // Serial.println("Parkir 1 Kosong");
+  }
+
+  if (digitalRead(Parkir_2) == LOW)
+  {
+    client.publish("Parkir/2", "1");
+    // Serial.println("Parkir 2 Terisi");
+  }
+  else
+  {
+    client.publish("Parkir/2", "0");
+    // Serial.println("Parkir 2 Kosong");
+  }
 }
